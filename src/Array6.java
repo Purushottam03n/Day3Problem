f25952e89035208c1e76ae7ad1bd9fb3807ddbe6
Purushottam <purushottam03n@gmail.com>
2022-09-17 16:13:35 +0530

    array6

---
+++ src/Array6.java
@@ -0,0 +1,9 @@
+public class Array6 {
+    public static void main(String[] args){
+        int[] arr = {10,20,30,40,50,60,70,80,90};
+        int i;
+        for(i=arr.length-1; i>=0 ; i-- ){
+            System.out.println(arr[i]+ " ");
+        }
+    }
+}
