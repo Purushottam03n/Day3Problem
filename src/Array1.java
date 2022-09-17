ecf373626dd2ae1d79d6341564fb438d1c26bd7e
Purushottam <purushottam03n@gmail.com>
2022-09-17 15:13:37 +0530

    Array1

---
+++ src/Array1.java
@@ -0,0 +1,16 @@
+public class Array1 {
+    public static void main(String[] args){
+        int[] arr;
+        arr = new int[5];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr [3] = 20;
+        arr[4] = 99;
+        for(int i=0; i<arr.length; i++){
+            System.out.println("Element in array: " +i);
+        }
+
+
+    }
+}
