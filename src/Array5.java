195309fd8338990cb73d10c2f67d2dffc0582d73
Purushottam <purushottam03n@gmail.com>
2022-09-17 15:56:19 +0530

    array5

---
+++ src/Array5.java
@@ -0,0 +1,14 @@
+public class Array5 {
+    public static void main(String[] args){
+
+        int[] arr = {20,40,50,60,30,80,90,55 };
+        int i;
+        for(i=0; i<arr.length; i++){
+            if(i%2==0){
+
+                System.out.println("Even Poisition are: " +arr[i]);
+            }
+
+        }
+    }
+}
